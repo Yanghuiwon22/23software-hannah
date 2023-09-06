8195ebe38e80a5a14aec5bdb44d4ee53e297ae5a
Yanghuiwon22 <hannah4212@naver.com>
2023-09-06 19:41:20 +0900

    .

--- hw02/lec04_prime_list.py
+++ hw02/lec04_prime_list.py
@@ -1,9 +1,14 @@
 def is_prime(num):
-    pass
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
 
+    return count == 2
 
 def main():
     list_prime = [x for x in range(1, 101) if is_prime(x)]
+    print(list_prime)
     print(f"1-100까지 중 소수는 {list_prime}입니다.")
     print(f"1-100까지 중 소수의 갯수는 {len(list_prime)}입니다.")
 
--- hw02/lec06_sum_even.py
+++ hw02/lec06_sum_even.py
@@ -1,13 +1,6 @@
-def get_prime(n,m):
-    get_list = [ i for i in range(n, m+1)]
-    for j in get_list:
-        pass
 
 def main():
-    first, last = map(int, input('몇부터 몇까지 고려할까요? ( 띄어쓰기로 구분해주세요. )').split(' '))
-    list_prime = [i for i in range(first, last) if get_prime(first, last)]
-    print(f'{list_prime}')
-
+    print(f'1-100까지 짝수의 합은 \033[36m{sum([x for x in range(1,101) if x%2==0])}\033[0m입니다.')
 
 if __name__=="__main__":
     main()

c05a48e74ca205d5b1f48b155f8a97a7cbf1e41f
Yanghuiwon22 <hannah4212@naver.com>
2023-09-06 19:33:33 +0900

    .

--- hw02/lec03_conv.py
+++ hw02/lec03_conv.py
@@ -1,14 +1,19 @@
 def is_prime(num):
-    return num%2==0
+    count = 0
+    for i in range(1,num+1):
+        if num%i == 0:
+            count += 1
+
+    return count == 2
 
 def main():
     while True:
         num = int(input('자연수를 입력하세요! '))
         if num > 0:
             if is_prime(num):
-                print(f"{num}은/는 소수입니다.")
+                print(f"{num}은/는 \033[36m소수\033[0m입니다.")
             else:
-                print(f"{num}은/는 소수가 아닙니다.")
+                print(f"{num}은/는 \033[36m소수가 아닙니다.")
             break
         else:
             print('\033[36m' + '자연수가 아닙니다!!!' + '\033[0m')

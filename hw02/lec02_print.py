679dc78068fa4d5885f5fccf516cb56a87669883
Yanghuiwon22 <hannah4212@naver.com>
2023-09-06 18:49:27 +0900

    .

--- hw02/lec02_print.py
+++ hw02/lec02_print.py
@@ -1,15 +1,14 @@
 def f2c(temp_f):
-    pass
+    return (temp_f - 32) * 5/9
 
-
+def c2f(temp_c):
+    return ( temp_c * 1.8 ) + 32
 def main():
-    temp_f = 80
-    # 두 줄
-    temp_c = f2c(temp_f)
-    print(f"{temp_f}F => {temp_c}℃")
+    temp_f = float(input('온도를 입력하세요(화씨) '))
+    temp_c = float(input('온도를 입력하세요(섭씨) '))
 
-    # 한 줄
     print(f"{temp_f}F => {f2c(temp_f)}℃")
+    print(f"{temp_c}℃ => {c2f(temp_c)}F")
 
 
 if __name__ == "__main__":
--- hw02/lec03_conv.py
+++ hw02/lec03_conv.py
@@ -1,15 +1,18 @@
 def is_prime(num):
-    pass
-
+    return num%2==0
 
 def main():
-    num = 3
-
-    if is_prime(num):
-        print(f"{num}은/는 소수입니다.")
-    else:
-        print(f"{num}은/는 소수가 아닙니다.")
-
+    while True:
+        num = int(input('자연수를 입력하세요! '))
+        if num > 0:
+            if is_prime(num):
+                print(f"{num}은/는 소수입니다.")
+            else:
+                print(f"{num}은/는 소수가 아닙니다.")
+            break
+        else:
+            print('\033[36m' + '자연수가 아닙니다!!!' + '\033[0m')
 
 if __name__ == "__main__":
     main()
+

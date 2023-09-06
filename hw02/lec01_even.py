284d6ee940761eff179f5ecc46e52838beb75f08
Yanghuiwon22 <hannah4212@naver.com>
2023-09-06 20:49:39 +0900

    .

--- hw02/lec01_even.py
+++ hw02/lec01_even.py
@@ -1,10 +1,9 @@
 def main():
-    # a = int(input("숫자를 입력하세요."))
-    a = 3
+    a = int(input("숫자를 입력하세요."))
     if a % 2 == 0:
-        print(f"{a}은/는 짝수입니다.")
+        print(f"{a}은/는 \033[36m짝수\033[0m입니다.")
     else:
-        print(f"{a}은/는 홀수입니다.")
+        print(f"{a}은/는 \033[36m홀수\033[0m입니다.")
 
 
 if __name__ == "__main__":

5c07dbecb0c2cc526725613f836e7dc214d784c9
mj4247 <minjai4247@naver.com>
2023-11-15 12:04:11 +0900

    weather
    weather

--- hw15/weather.py
+++ hw15/weather.py
@@ -1,13 +1,30 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 import requests
 from pprint import pprint
 
-API_Key = "2377a6d257fdfc1cdc2b4c4e4f79a982"
+app = FastAPI()
 
-city = input(" 도시를 입력해주세요: ")
+API_KEY = "2377a6d257fdfc1cdc2b4c4e4f79a982"
 
-base_url = "https://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
 
-weather_data = requests.get(base_url).json()
+def get_weather_data(city):
+    base_url = f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={city}"
+    weather_data = requests.get(base_url).json()
+    return weather_data
 
-pprint(weather_data)
 
+@app.get("/weather/{city}")
+def get_weather(city: str):
+    weather_data = get_weather_data(city)
+    if "message" in weather_data and weather_data["cod"] == "404":
+        raise HTTPException(status_code=404, detail="City not found")
+
+    # 수동으로 들여쓰기를 적용하여 JSON 형식으로 반환
+    return JSONResponse(content=weather_data, media_type="application/json", status_code=200)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
